d1055ae8ebf1db424fff7a96c41979c71fba6672
yasuhisa <penmouse275@nifty.com>
2016-12-25 14:27:03 +0000

    Gemfile update for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161224083306) do
+
+  create_table "notes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text     "title"
+    t.string   "content"
+  end
 
 end
